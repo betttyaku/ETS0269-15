ce5ad7c4e2b916dc0b875de04fef7586046a152d
betttyaku <genzebbethelhem@gmail.com>
2025-03-26 06:35:05 -0700

    submitted 3 string method

--- string_method.py
+++ string_method.py
@@ -12,3 +12,18 @@ string ="python"
 string_title = string.title()
 print(string_title)
 #output: Python
+
+string ="bethelhem"
+string_capitalize =string.capitalize()
+print(string_capitalize)
+#output: Bethelhem
+
+string ="bethelhem" 
+string_swapcase = string.swapcase()
+print(string_swapcase)
+#output:BETHELHEM
+
+string = "betelhem"
+string_islower =string.islower()
+print(string_islower)
+#output:
