7fe08877cc847a075344fe3f6f03a8688e72d7be
betttyaku <genzebbethelhem@gmail.com>
2025-03-26 05:26:17 -0700

    submitted 3 string methods

---
+++ string_method.py
@@ -0,0 +1,14 @@
+string = "BLUE"
+
+string_lowercase = string.lower()
+print(string_lowercase)
+#Output: blue
+
+string_uppercase = string_lowercase.upper()
+print(string_uppercase)
+#Output: BLUE
+
+string ="python"
+string_title = string.title()
+print(string_title)
+#output: Python
