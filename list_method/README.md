8cceebbf94c111fb3b27c79507d8565343940d1a
betttyaku <genzebbethelhem@gmail.com>
2025-04-01 01:43:00 -0700

    adding the 3 list  method and readme

--- list_method/README.md
+++ list_method/README.md
@@ -1,3 +1,6 @@
 -'append()': Adds an element to the end of the list.
 -'copy()': Returns a shallow copy of the list.
 -'clear()': Removes all elements from the list.
+#count ():returns the number of times a specified element appears in the list.
+#extend ():ADD elements from others list to a current list.
+#index ():a number specifies the postion of a given list.
--- list_method/listmethods.py
+++ list_method/listmethods.py
@@ -9,3 +9,18 @@ print(my_list)
 my_list = ["2","3","4","5"]
 my_list.copy()
 print(my_list)
+
+my_list = ["1","2","3","4","5"]
+my_list.count("3")
+print(my_list)
+#output : ['1', '2', '3', '4', '5']
+
+my_list = ["a","d","f","j","k"]
+my_list.extend("n")
+print(my_list)
+#output : ['a', 'd', 'f', 'j', 'k', 'n']
+
+my_list =  ["1","2","3","4","5"]
+my_list.index("1")
+print(my_list)
+#output :['1', '2', '3', '4', '5'] 
