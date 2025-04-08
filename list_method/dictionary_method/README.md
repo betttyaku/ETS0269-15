416c29e553af51f60760d22699e368660122731a
betttyaku <genzebbethelhem@gmail.com>
2025-04-08 09:21:29 -0700

    adding the 3 dictionary  method and readme

---
+++ list_method/dictionary_method/README.md
@@ -0,0 +1,3 @@
+#clear():remove all element from dictionary.
+#copy():returns a copy of dictionary.
+#get():return the value of the specified key.
---
+++ list_method/dictionary_method/dictionary.py
@@ -0,0 +1,22 @@
+my_dict = {
+    "name":"betty",
+    "age":"20",
+    "sex":"female"}
+
+my_dict.clear()
+print(my_dict)
+
+my_dict = {
+    "name":"betty",
+    "age":"20",
+    "sex":"female"}
+my_dict.copy()
+print(my_dict)
+
+my_dict={'a':1,'b':2,'c':3}
+my_dict.get('a')
+print(my_dict)
+
+
+
+
