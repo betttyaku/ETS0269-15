a4709a0a6159008d45d750e0dc7d413c6b8eb7fe
betttyaku <genzebbethelhem@gmail.com>
2025-04-18 19:02:51 -0700

    submitted tuple  methods

---
+++ tuple_method/README.md
@@ -0,0 +1,3 @@
+#tuple count mean tallies how many times an element appers.
+#tuple add mean allowes to add some values.
+#tuple index mean return the index value of the first occurrence of the element.
---
+++ tuple_method/tuple_method.py
@@ -0,0 +1,21 @@
+my_tuple =('1','2','4','5','6')
+
+print(my_tuple.count('2'))
+
+my_tuple =('1','2','4','5','6')
+print(my_tuple.index('4'))
+
+my_tuple =('1','2','4','5','6')
+my_tuple.__add__
+print(my_tuple)
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
