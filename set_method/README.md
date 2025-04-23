ccf5928b882b29d223e2ef8ed4a662b22a94f39a
betttyaku <genzebbethelhem@gmail.com>
2025-04-22 18:47:59 -0700

    submitted set  methods

---
+++ set_method/README.md
@@ -0,0 +1,3 @@
+#set add mean adding new value to a given set.
+#set clear mean remove all elements then displaying clean set.
+#set remove mean removing specific element from the set.
---
+++ set_method/set_method.py
@@ -0,0 +1,11 @@
+set_1= {1,2,3,4,5,6,7,8,}
+set_1.remove(8)
+print(set_1)
+
+set_1= {1,2,3,4,5,6,7,8,}
+set_1.add(9)
+print(set_1)
+
+set_1= {1,2,3,4,5,6,7,8,}
+set_1.clear()
+print(set_1)
