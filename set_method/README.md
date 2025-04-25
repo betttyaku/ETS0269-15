f80a53dbf5bfbd1957ceeed5d2772793f768b8a0
betttyaku <genzebbethelhem@gmail.com>
2025-04-24 17:41:27 -0700

    submitted set  methods

--- set_method/README.md
+++ set_method/README.md
@@ -1,3 +1,6 @@
 #set add mean adding new value to a given set.
 #set clear mean remove all elements then displaying clean set.
 #set remove mean removing specific element from the set.
+#set copy mean return a copy of all set.
+#set difference mean return the difference between 2 or more set.
+#set intersection mean return the common element of all sets.
--- set_method/set_method.py
+++ set_method/set_method.py
@@ -9,3 +9,17 @@ print(set_1)
 set_1= {1,2,3,4,5,6,7,8,}
 set_1.clear()
 print(set_1)
+
+set_1= {1,2,3,4,5,6,7,8,}
+set_1.copy()
+print(set_1)
+
+set_1= {1,2,3,4,5,6,7,8,}
+set_2 ={1,9,0,8,6,4}
+difference = set_1.difference(set_2)
+print(difference)
+
+set_1= {1,2,3,4,5,6,7,8,}
+set_2 ={1,6,7,0,9,5}
+intersection = set_1.intersection(set_2)
+print(intersection)
